5fcc1565d00dc3fbd3fa754ab1fdb59ec2129d24
KawaiHaruki <haruki.kawai.1999@gmail.com>
2025-06-12 23:24:27 +0900

    initial commit

---
+++ test/color-match-puzzle.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ColorMatchPuzzle from '../lib/color-match-puzzle-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/color-match-puzzle-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ColorMatchPuzzle.ColorMatchPuzzleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
